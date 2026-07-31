21d0a0aa3c06e17ea381182f9edd46c5e7152f28
rajeshtct96 <rajeshtct96@gmail.com>
2026-07-31 22:38:16 +0530

    create new project

---
+++ src/main/java/com/ptms/PtmsErpApplication.java
@@ -0,0 +1,13 @@
+package com.ptms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtmsErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtmsErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ptms/PtmsErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtmsErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
